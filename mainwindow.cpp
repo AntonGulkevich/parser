ea5fa8e453a79e66909b5869df27c6160bf92282
Anton Gulkevich <ycykensmit@gmail.com>
2015-08-19 15:28:24 +0300

    added auto mode
    need to change logic in filenames

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,16 +5,39 @@
 #define SMALLBUTTONSIZE 30
 #define MAXH 190
 #define MAXW 440
+#define HEADER_SIZE 256
 
 
-
-MainWindow::MainWindow(QWidget *parent)
-    : QFrame(parent)
+MainWindow::MainWindow(QWidget *parent):QFrame(parent)
 {
     initDefaultStyle();
-
+    initDefVars();
     initContent();
 
+    autoStart(pathToFolder+"commod.bin");
+
+
+}
+
+bool MainWindow::autoStart(const QString &fileName)
+{
+   onOpenFileDialogFinished(fileName);
+   return true;
+}
+
+bool MainWindow::openFile(const QString &fileName)
+{
+    //stream binary read
+    currentFile = new QFile (fileName);
+    if (!currentFile->open(QIODevice::ReadOnly))
+    {        
+        currentFile->close();
+        logEdit->append("Unable to open file.");
+        currentFile = NULL;
+        return false;
+    }
+    appendToLog("File "+ fileName+ " opened.");
+    return true;
 }
 
 void MainWindow::initDefaultStyle(){
@@ -52,19 +75,16 @@ void MainWindow::initDefaultStyle(){
 void MainWindow::initContent(){
 
     openFileButton = new QPushButton(this);
-    openFileButton->setVisible(true);
     openFileButton->setGeometry(PADDING, PADDING, BUTTONSIZE, BUTTONSIZE);
-    openFileButton->setText("Open File");
+    openFileButton->setText("Convert File");
     openFileButton->setToolTip("Open single file to parse");
 
     openDirButton = new QPushButton(this);
-    openDirButton->setVisible(true);
     openDirButton->setGeometry(MAXH, PADDING, BUTTONSIZE, BUTTONSIZE);
-    openDirButton->setText("Open Directory");
+    openDirButton->setText("Convert files\nin directory");
     openDirButton->setToolTip("Open all files in current folder");
 
     closeButton = new QPushButton(this);
-    closeButton->setVisible(true);
     closeButton->setGeometry(MAXW-SMALLBUTTONSIZE*2-PADDING, PADDING, SMALLBUTTONSIZE*2, SMALLBUTTONSIZE);
     closeButton->setText("close");
     closeButton->setToolTip("Close programm");
@@ -74,31 +94,160 @@ void MainWindow::initContent(){
                                "background-color: #800000;"
                                "color: white}"
                                "QPushButton:hover{"
-                               "background-color: #990000}");
+                               "background-color: #B30000}");
 
     optionsButton = new QPushButton(this);
-    optionsButton->setVisible(true);
     optionsButton->setGeometry(MAXW-SMALLBUTTONSIZE*2-PADDING, PADDING*2 +SMALLBUTTONSIZE, SMALLBUTTONSIZE*2, SMALLBUTTONSIZE*2);
-    optionsButton->setText("options");
-    optionsButton->setToolTip("Setup");
+    optionsButton->setText("setup");
+    optionsButton->setToolTip("setup");
 
-    optionsButton = new QPushButton(this);
-    optionsButton->setVisible(true);
-    optionsButton->setGeometry(MAXW-SMALLBUTTONSIZE*2-PADDING, PADDING*3 +SMALLBUTTONSIZE*3, SMALLBUTTONSIZE*2, SMALLBUTTONSIZE*2);
-    optionsButton->setText("log");
-    optionsButton->setToolTip("Show log");
+    logButton = new QPushButton(this);
+    logButton->setGeometry(MAXW-SMALLBUTTONSIZE*2-PADDING, PADDING*3 +SMALLBUTTONSIZE*3, SMALLBUTTONSIZE*2, SMALLBUTTONSIZE*2);
+    logButton->setText("log");
+    logButton->setToolTip("Show log");
 
-    pathToFolder = "//fs/Group Projects/UBS/База конфигураций";//QStandardPaths::locate(QStandardPaths::DesktopLocation, QString(), QStandardPaths::LocateDirectory)
+    //pathToFolder = "//fs/Group Projects/UBS/База конфигураций";//
+    pathToFolder = QStandardPaths::locate(QStandardPaths::DesktopLocation, QString(), QStandardPaths::LocateDirectory);
 
     openDirDlg = new QFileDialog(this,tr("Open file"), pathToFolder);
     openFileDlg = new QFileDialog(this,tr("Open file"), pathToFolder, tr("bin files (*.bin)"));
 
     openDirDlg->setFileMode(QFileDialog::DirectoryOnly);
 
+    logEdit = new QTextEdit(this);
+    logEdit->setGeometry(PADDING, MAXH, MAXW-PADDING*2, MAXH-PADDING);
+    logEdit->setReadOnly(true);
+    logEdit->setVisible(false);
+    logEdit->setStyleSheet("border-style:solid; border-color:white;border-width:1px; color:white");
+    appendToLog("Programm started");
+
+
     connect(closeButton, SIGNAL(clicked(bool)), this, SLOT(onCloseButtonClicked()));
     connect(openDirButton, SIGNAL(clicked(bool)), openDirDlg, SLOT(open()));
     connect (openFileButton, SIGNAL(clicked(bool)), openFileDlg, SLOT(open()));
+    connect (logButton, SIGNAL(clicked(bool)), this, SLOT(onOpenLogClicked()));
+    connect (openFileDlg, SIGNAL(fileSelected(QString)), this, SLOT(onOpenFileDialogFinished(QString)));
+}
 
+void MainWindow::initDefVars()
+{
+    currentFile= NULL;
+}
+
+bool MainWindow::saveToFile(const QString &fileName, const QString &src)
+{
+    QTextCodec *codec = QTextCodec::codecForName("Windows-1251");
+    QByteArray encodedString = codec->fromUnicode(src);
+
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly))
+    {
+        appendToLog("Unable to save file"+ fileName);
+        file.close();
+        return false;
+    }
+    QDataStream out(&file);
+    out.writeRawData(encodedString.data(), encodedString.size());
+    file.close();
+    appendToLog("File" + fileName + " saved");
+    return true;
+
+}
+
+void MainWindow::parseCurrentFile()
+{
+    appendToLog("Parsing started.");
+    /*parsing header to vector*/
+    QDataStream in(currentFile);
+
+    QByteArray header;
+    header.resize(HEADER_SIZE);
+    in.readRawData(header.data(), HEADER_SIZE);
+    QVector <Fat> headerVector;
+    for (int i = 0; (header[i] != 0xFF && i < 64); i+=4)
+    {
+        Fat f;
+        f.address = (static_cast<qint32>(header.at(i) & 0xFF) <<8)
+                    + (static_cast<qint32>(header.at(i+1)) <<16);
+        f.size =  (static_cast<qint32>(header.at(i+2)) & 0xFF)
+                 + (static_cast<qint32>(header.at(i+3))<<8);
+        headerVector.push_back(f);
+    }
+    /*end of parding header*/
+
+    /*parsing data*/
+    QByteArray dataToParse;
+    int dataSize;
+    dataSize = currentFile->size()-HEADER_SIZE;
+    dataToParse.resize(dataSize);
+    in.readRawData(dataToParse.data(), dataSize);
+
+    QTextCodec *codec = QTextCodec::codecForName("Windows-1251");
+    QString textData = codec->toUnicode(dataToParse);
+    QStringList dataByParts = textData.split("GENERAL");
+    dataByParts.removeFirst();//the firts part is always zero
+
+    for (int i=0; i<dataByParts.count();++i)
+    {
+        /*kav names*/
+        QString temp(dataByParts[i]);
+        temp.simplified();
+        temp=temp.left(temp.lastIndexOf("\r\nEND\r\n")+7);
+        //temp+="\r\nEND\r\n";
+        temp.replace(QString(" = "), QString("="));
+        int charsToDel=0;
+        int start =0;
+        int tempCount=temp.size()-1;
+        for (int j=0;j<tempCount;++j)
+        {
+            if (temp[j]=='/'&&temp[j+1]=='/')
+            {
+                start=j;
+                charsToDel=0;
+                while (temp[j++]!='\r')
+                    charsToDel++;
+
+                temp.remove(start, charsToDel);
+                j-=charsToDel;
+                tempCount=tempCount-charsToDel;
+            }
+        }
+        tempCount=temp.count()-1;
+        for (int j=0;j<tempCount;++j)
+        {
+            if (temp[j]==' '&&temp[j+1]==' ')
+            {
+                start=j;
+                charsToDel=0;
+                while (temp[j++]==' ')
+                    charsToDel++;
+
+                temp.remove(start, charsToDel);
+                j-=charsToDel;
+                tempCount=tempCount-charsToDel;
+            }
+
+        }
+        temp.simplified();
+        dataByParts[i]= temp;
+    }
+
+    QString teststr=dataByParts.join("\r\nGENERAL\r\n");
+    saveToFile(currentFile->fileName()+"t", teststr);
+
+    /*end of data parsing*/
+
+    /*text part must be multiply of 256*/
+
+    currentFile->close();
+    currentFile= NULL;
+
+}
+
+void MainWindow::appendToLog(const QString &text)
+{
+    dateTime = QDateTime::currentDateTime();
+    logEdit->append(dateTime.toString("dd.MM.yyyy hh:mm:ss ")+ text);
 }
 
 MainWindow::~MainWindow()
@@ -106,6 +255,7 @@ MainWindow::~MainWindow()
     delete openDirButton;
     delete openFileButton;
     delete openDirDlg;
+    delete openFileDlg;
 }
 
 void MainWindow::onCloseButtonClicked()
@@ -121,5 +271,47 @@ bool MainWindow::onOpenFileButtonClicked()
 
 bool MainWindow::onOpenDirButtonClicked()
 {
+
     return true;
+}
+
+void MainWindow::onOpenFileDialogFinished(QString fileName)
+{
+    openFile(fileName);
+    parseCurrentFile();
+}
+
+void MainWindow::onOpenLogClicked()
+{
+    logEdit->setVisible(!logEdit->isVisible());
+    if (logEdit->isVisible()){
+        mainWindowSize.setHeight(MAXH*2);
+    }
+    else{
+        mainWindowSize.setHeight(MAXH);
+    }
+    resize(mainWindowSize);
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton)
+    {
+        canMove = true;
+        mpos = event->pos();
+    }
+}
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if((event->buttons() & Qt::LeftButton) && canMove)
+    {
+        move(event->globalPos()-mpos);
+    }
+}
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton && canMove)
+    {
+        canMove = false;
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,7 +14,14 @@
 #include <QDebug>
 #include <QAbstractAnimation>
 #include <QString>
-
+#include <QMouseEvent>
+#include <QFile>
+#include <QTextStream>
+#include <QTextCodec>
+#include <QStringList>
+#include <QVector>
+#include <QTextEdit>
+#include <QDateTime>
 
 class MainWindow : public QFrame
 {
@@ -22,28 +29,52 @@ class MainWindow : public QFrame
 private:
     QPoint mainWindownStartPoint;
     QSize mainWindowSize;
+    QDateTime dateTime;
 
     QPushButton *openFileButton;
     QPushButton *openDirButton;
     QPushButton *closeButton;
     QPushButton *optionsButton;
-    QPushButton *log;
+    QPushButton *logButton;
+    QTextEdit * logEdit;
 
     QFileDialog *openDirDlg;
     QFileDialog *openFileDlg;
 
     QString pathToFolder;
+    QFile *currentFile;
+
+    QPoint mpos;
+    bool canMove;
 
     void initDefaultStyle();
     void initContent();
+    void initDefVars();
+    bool saveToFile(const QString &fileName, const QString &src);
+    void parseCurrentFile();
+    void appendToLog(const QString &text);
+    bool openFile(const QString &fileName);
+
+    struct Fat{
+        quint32 address;
+        quint32 size;
+    };
 
 public:
-    MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = 0);
+    bool autoStart(const QString & fileName);
+
     ~MainWindow();
 private slots:
     void onCloseButtonClicked();
     bool onOpenFileButtonClicked();
     bool onOpenDirButtonClicked();
+    void onOpenFileDialogFinished(QString fileName);
+    void onOpenLogClicked();
+protected:
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
 };
 
 #endif // MAINWINDOW_H
